dfd3768e9db03b61fae6592b60dd98edf8233383
KarelVerkleij <karel.verkleij@gmail.com>
2024-10-25 14:34:17 +0200

    comments

--- snake/main.py
+++ snake/main.py
@@ -1,3 +1,8 @@
+"""
+sourced from http://pygametutorials.wikidot.com/tutorials-basic
+sourced from https://www.geeksforgeeks.org/snake-game-in-python-using-pygame-module/ 
+"""
+
 import pygame
 
 class App:
@@ -6,33 +11,40 @@ class App:
         self._display_surf = None
         self.size = self.weight, self.height = 640, 400
 
+    # initializes all pygame modules, then creates window 
     def on_init(self):
         pygame.init()
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._running = True
 
+    # check if game was quit
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
 
+    # do nothing
     def on_loop(self):
         pass
 
+    # do nothing
     def on_render(self):
         pass
     
+    # quits all pygame modules
     def on_cleanup(self):
         pygame.quit()
     
+    # initializes pygame & enters main loop where events are checked, exectued in a continuous cycle
     def on_execute(self):
         if self.on_init() == False:
-            self._running = False
+            self._running = False 
 
         while( self._running ):
             for event in pygame.event.get():
                 self.on_event(event)
             self.on_loop()
             self.on_render()
+
         self.on_cleanup()
 
 if __name__ == '__main__':
